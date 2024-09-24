58ae0bf50beb2f118b04371b605e22a3a49596dc
Deniza-lab <dewill177@duck.com>
2024-09-24 21:02:50 +1200

    trying to install bootstrap, added pages, installed packages
    had to restart the project after failing to download tailwind

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 app.set('view engine','ejs');
+app.use('/public', express.static('public'));
+
+//PAGES
 app.get('/', function (req, res){
-    res.render("home");
-});
+res.render("home");
+});   
+app.get('/emotions', function (req, res){
+    res.render("emotions");
+    }); 
+app.get('/skills', function (req, res){
+    res.render("skills"); 
+}); 
+
 app.listen(3000);
 console.log('Node app is running on port 3000');
